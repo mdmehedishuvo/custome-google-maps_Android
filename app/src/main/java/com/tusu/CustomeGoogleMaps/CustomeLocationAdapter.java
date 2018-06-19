d5aed03bca388bc9cb50606d576188ba33a78408
shuvo <bdmehedishuvo@gmail.com>
2018-06-19 22:58:55 +0600

    Initial commit

---
+++ app/src/main/java/com/tusu/CustomeGoogleMaps/CustomeLocationAdapter.java
@@ -0,0 +1,53 @@
+package com.tusu.CustomeGoogleMaps;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Created by User on 10/2/2017.
+ */
+
+public class CustomeLocationAdapter implements GoogleMap.InfoWindowAdapter {
+
+    private final View mWindow;
+    private Context mContext;
+
+    public CustomeLocationAdapter(Context context) {
+        mContext = context;
+        mWindow = LayoutInflater.from(context).inflate(R.layout.custome_location_info, null);
+    }
+
+    private void rendowWindowText(Marker marker, View view){
+
+        String title = marker.getTitle();
+        TextView tvTitle = (TextView) view.findViewById(R.id.title);
+
+        if(!title.equals("")){
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = (TextView) view.findViewById(R.id.snippet);
+
+        if(!snippet.equals("")){
+            tvSnippet.setText(snippet);
+        }
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+}
